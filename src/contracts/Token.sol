95509236c0a7f77681527923516dc3e700729e11
Alex Roan <alex.roan@hotmail.com>
2019-10-29 17:37:23 +0000

    Added 3rc20 tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,4 +2,11 @@ pragma solidity ^0.5.0;
 
 contract Token {
     string public name = "Token Name";
+    string public symbol = "TOK";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,15 +1,41 @@
-const Token = artifacts.require('./Token')
+const Token = artifacts.require('./Token');
 
 require('chai')
     .use(require('chai-as-promised'))
-    .should()
+    .should();
 
 contract('Token', (accounts) => {
+    let token;
+    beforeEach(async () => {
+        token = await Token.new();
+    });
+
     describe('deployment', () => {
-        it('gets the name', async () => {
-            const token = await Token.new()
-            const result = await token.name()
-            result.should.equal("Token Name")
-        })
-    })
+        const name = "Token Name";
+        const symbol = "TOK";
+        const decimals = 18;
+        const totalSupply = 1000000000000000000000000;
+
+        it('tracks the name', async () => {
+            const result = await token.name();
+            result.should.equal(name);
+        });
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol();
+            result.should.equal(symbol);
+        });
+
+        it('tracks the decimals', async () => {
+            const result = await token.decimals();
+            const parseResult = parseInt(result);
+            parseResult.should.equal(decimals);
+        });
+
+        it('tracks the total supply', async () => {
+            const result = await token.totalSupply();
+            const parseResult = parseInt(result);
+            parseResult.should.equal(totalSupply);
+        });
+    });
 });
