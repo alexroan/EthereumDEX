8265c2425da02f25a4f9a87b20b4497a4533c960
Alex Roan <alex.roan@hotmail.com>
2019-10-29 17:15:28 +0000

    Added first test

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,7 +1,15 @@
-const Token = artifacts.require('./Token');
+const Token = artifacts.require('./Token')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
 
 contract('Token', (accounts) => {
     describe('deployment', () => {
-        
+        it('gets the name', async () => {
+            const token = await Token.new()
+            const result = await token.name()
+            result.should.equal("Token Name")
+        })
     })
 });
