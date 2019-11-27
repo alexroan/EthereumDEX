d574168664762073345af9b839d2f9b47dd98063
Alex Roan <alex.roan@hotmail.com>
2019-11-27 16:50:15 +0000

    Updates

---
+++ precache-manifest.83aedbc01ba2abdb3dbd5812f7ce492d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d47665724b395c094e2",
+    "url": "/EthereumDEX/static/js/runtime~main.2d476657.js"
+  },
+  {
+    "revision": "52c5a1a9b3b16ea792de",
+    "url": "/EthereumDEX/static/js/main.52c5a1a9.chunk.js"
+  },
+  {
+    "revision": "abec2bb6a86f9cf86c5e",
+    "url": "/EthereumDEX/static/js/1.abec2bb6.chunk.js"
+  },
+  {
+    "revision": "52c5a1a9b3b16ea792de",
+    "url": "/EthereumDEX/static/css/main.5b8d91b6.chunk.css"
+  },
+  {
+    "revision": "abec2bb6a86f9cf86c5e",
+    "url": "/EthereumDEX/static/css/1.03d0afb5.chunk.css"
+  },
+  {
+    "revision": "be623b0c1336461b2d8774f097f212b6",
+    "url": "/EthereumDEX/index.html"
+  }
+];
