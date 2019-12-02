164c276591d469fc81141101f0fa09eba973c14d
Alex Roan <alex.roan@hotmail.com>
2019-12-02 09:33:04 +0000

    Removed options from the token selections in navbar until loading different erco20 tokens is supported

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,9 +21,9 @@ class Navbar extends Component {
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                            {/* <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                             <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item> */}
                         </Dropdown.Menu>
                     </Dropdown>
                     </li>
