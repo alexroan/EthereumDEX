d55bf2d437ed66f68cb84fc9de07462286a205d8
Alex Roan <alex.roan@hotmail.com>
2020-02-05 16:57:18 +0000

    Added 2 zeros to minting

--- src/contracts/MyERC20.sol
+++ src/contracts/MyERC20.sol
@@ -5,6 +5,6 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract MyERC20 is ERC20, ERC20Detailed {
     constructor () public ERC20Detailed("MyERC20", "MERC", 18) {
-        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
